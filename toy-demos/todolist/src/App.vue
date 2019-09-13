e15e72dfa97c60d09bc1c8715e9a2d00f0659e61
Bingyy <bing@bingw.me>
2019-09-13 21:01:00 +0800

    add todolist

---
+++ toy-demos/todolist/src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="app">
+    <Todos />
+  </div>
+</template>
+
+<script>
+import Todos from './components/Todos';
+export default {
+  name: 'app',
+
+  components: {
+    Todos,
+  },
+  data() {
+    return {
+      todos: [
+        {
+          id: 1,
+          title: "待办事项1",
+          completed: true,
+        },
+        {
+          id: 2,
+          title: "待办事项2",
+          completed: true,
+        },
+        {
+          id: 3,
+          title: "待办事项3",
+          completed: true,
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  line-height: 1.4;
+}
+</style>
---
+++ toy-demos/todolist/src/components/Todos.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h1>Todos</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Todos",
+    props: {
+
+    },
+    components: {
+
+    },
+    methods: {
+
+    },
+}
+</script>
+<style scoped>
+    
+</style>
